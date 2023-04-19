a311f94c435500c8b7e62d444f7fa7315bec1ffd
pepe <kousaku5963@gmail.com>
2023-04-20 02:03:20 +0900

    AL3_02_01_3Dレールシューティングの課題です

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+	win->CreateGameWindow(L"LE2B_22_フミモト_コウサク");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
