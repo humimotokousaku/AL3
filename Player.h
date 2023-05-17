0b2c097e69412ec4ca82354baa58b94ff56b4a43
pepe <kousaku5963@gmail.com>
2023-05-17 15:26:36 +0900

    Matrixの計算をファイル分けした

--- Player.h
+++ Player.h
@@ -9,33 +9,6 @@ public:
 	Player();
 	~Player();
 
-#pragma region Matrix
-
-	// X軸回転行列
-	Matrix4x4 MakeRotateXMatrix(float radius);
-	// Y軸回転行列
-	Matrix4x4 MakeRotateYMatrix(float radius);
-	// Z軸回転行列
-	Matrix4x4 MakeRotateZMatrix(float radius);
-
-	// 行列の積
-	Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
-
-	// 平行移動行列
-	Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
-
-	// 拡大縮小行列
-	Matrix4x4 MakeScaleMatrix(const Vector3& scale);
-
-	// 座標変換
-	Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
-
-	// 3次元アフィン変換行列
-	Matrix4x4
-	    MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-
-#pragma endregion
-
 	/// <summary>
 	/// 初期化
 	/// <summary>
